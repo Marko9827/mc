7b6d0b3b203c9b9ac58a6d599c1c514fd68746df
Marko Nikolic <marko.supergun@gmail.com>
2023-06-17 18:54:09 +0200

    lang json

---
+++ src/app/actions/lang_en.ts
@@ -0,0 +1,8 @@
+export default  function lang(hmm = "") {
+    const Arrlang = [{
+        "error_login_pu":"Please type correct password and email"
+    }];
+    
+ 
+    
+}
--- src/pages/api/auth/[...nnextauth].ts
+++ src/pages/api/auth/[...nnextauth].ts
@@ -1,30 +1,50 @@
-import { AuthOptions } from "next-auth";
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import NextAuth from "next-auth/next";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
+import prisma from "@/app/libs/prismadb";
 import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from '@/app/libs/prismadb';
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+
+import lang from "@/app/actions/lang_en";
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers:[
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-              email: { label: "email", type: "text" },
-              password: { label: "password", type: "password" },
-            }
-        )
-    ],
-    pages: {
-        signIn: '/',
-        signOut: '/logout'
-    },
-    debug: process.env.NODE_ENV === 'development',
-    session: {
-        strategy: "jwt"
-    },
-    secret: process.env.NEXTAUTH_SECRET
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Please type correct password and email");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user?.hashedPasword) {
+          throw new Error("Error login details");
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPasword
+        );
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+    signOut: "/logout",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
