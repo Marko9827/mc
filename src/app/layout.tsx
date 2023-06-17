207dbe7fda8e1775b13c957461ad24161c82c4d3
Marko Nikolic <marko.supergun@gmail.com>
2023-06-17 15:11:29 +0200

    fix env
    ignore .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import ClientOnly from './components/clientOnly'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import navbar from './components/navbar/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ClientOnly> 
+        </ClientOnly>
+        {children}</body>
     </html>
   )
 }
