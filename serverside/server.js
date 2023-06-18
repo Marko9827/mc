bc056d006f8691f1105e881ae1a7238c2f636170
Marko Nikolic <marko.supergun@gmail.com>
2023-06-18 15:26:34 +0200

    serverside update mysql connection

--- serverside/server.js
+++ serverside/server.js
@@ -8,5 +8,19 @@ const express = require("express"),
 
   const api = process.env.API_URL;
  
-  
+  app.use(express.json()); 
+  const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS
+  });
+
+  con.connect(function(err){
+    if(err){
+      console.log("Connection error");
+    } else{
+      console.log("SQL OK.");
+    }
+  })
  
