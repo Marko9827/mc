c4ef5bf63095492dc30879b675760d1af560925a
Marko Nikolic <marko.supergun@gmail.com>
2023-06-18 11:02:35 +0200

    reg

--- src/app/components/modals/Registermodal.tsx
+++ src/app/components/modals/Registermodal.tsx
@@ -1,12 +1,27 @@
 import useRegisterModal from '@/app/components/hoooooks/Useregistermodal'
 import { useCallback, useState } from "react"
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+
 
 const RegisterModal = () => {
 
     const registerModal = useRegisterModal();
 
     const [isLoading, setIsLoading] = useState(false);
-    
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    })
 }
 
 export default RegisterModal
